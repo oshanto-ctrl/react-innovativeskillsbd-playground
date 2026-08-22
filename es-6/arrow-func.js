faebb11bc1c2cda702c08c5e92a002c10aec60f1
Rejoan Siddiky <siddikyrejoan@gmail.com>
2026-08-23 04:53:42 +0600

    es6 arrow func practice

---
+++ es-6/arrow-func.js
@@ -0,0 +1,59 @@
+/*
+
+const functionName = (params) => {
+        // function body
+        // return result ( explicitly returns a value.)
+    };
+
+*/
+
+// Takes two numbers as params and return their sum.
+// Can have multiple params.
+const add = (a, b) => a + b;
+console.log(add(5, -7)); // - 2 
+
+
+// without params
+const gfg = () => {
+    console.log("Hello from GFG.");
+}; 
+gfg(); // Called gfg() here.
+
+// Arrow function with single param
+// parentheses around params omitted
+// Used in callbacks, array methods, simple ops
+const square = x => x*x;
+console.log("Square of 4 is ", square(4));
+
+
+// In JS returning object literals within
+// functions is concise: () => ({key: value}) retuns
+// an object {key: value}
+// useful on immediate object create & return
+const makeUser = (userName, userEmail) => (
+    {
+        userName: userName,
+        userEmail: userEmail,
+    });
+
+console.log("Making user: ", makeUser("Rasel", "rejaul@xmail.com"));
+
+
+// Async Arrow Functions use of await for wait for promise resolve
+const fetchData = async () => {
+    const data = await fetch("https://fake-json-api.mock.beeceptor.com/companies/7");
+    return data.json();
+};
+
+console.log("Random data = ", await fetchData());
+
+
+
+
+
+
+
+
+
+
+
